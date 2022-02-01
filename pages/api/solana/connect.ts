feb467db918fbacdda11ac1ba08325a0f1b492b7
Abhi Pillai <abhi.pillai1@gmail.com>
2022-02-01 14:25:46 -0500

    Learning solana dev through figment learn.
    so far, have created an account with a keypair and funded the account with SOL

--- pages/api/solana/connect.ts
+++ pages/api/solana/connect.ts
@@ -9,8 +9,8 @@ export default async function connect(
   try {
     const {network} = req.body;
     const url = getNodeURL(network);
-    const connection = undefined;
-    const version = undefined;
+    const connection = new Connection('http://127.0.0.1:8899');
+    const version = await connection.getVersion();
     res.status(200).json(version['solana-core']);
   } catch (error) {
     let errorMessage = error instanceof Error ? error.message : 'Unknown Error';
